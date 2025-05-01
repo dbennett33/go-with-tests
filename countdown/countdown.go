8e1d2ede057dedc6ecc5f0b957460b854b2242ed
Dan <danbennett2k8@gmail.com>
2025-05-01 22:12:26 +0100

    Refactor Countdown implementation to use ConfigurableSleeper; update tests for consistency and clarity

--- countdown/countdown.go
+++ countdown/countdown.go
@@ -11,7 +11,7 @@ const finalWord = "Go!"
 const countdownStart = 3
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
 
@@ -37,12 +37,6 @@ func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
 
-type DefaultSleeper struct{}
-
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
-}
-
 type SpyCountdownOperations struct {
 	Calls []string
 }
@@ -58,3 +52,20 @@ func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
 
 const write = "write"
 const sleep = "sleep"
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
